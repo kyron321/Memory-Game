98efb69a1c663e4cdffb6b1354cc152601407e3b
kyron321 <kds.060902@gmail.com>
2022-10-30 17:44:10 +0000

    Created memory game project base

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import {useState} from 'react'
+
 
 function App() {
-const [name, setName] = useState('Carmen Winstead') 
-console.log(name)
-
-// const handleClick = () => {
-//   setName("kyron smith")
-// }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      {/* <button onClick={handleClick}>Change name</button> */}
-      <button onClick={()=>setName("Kyron Smith")}>Change Name</button>
+      <h1>Magic Match</h1>
+      <button>New Game</button>
     </div>
   );
 }
