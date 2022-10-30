efb16b307a45975405633efb5e742132b00c18ac
kyron321 <kds.060902@gmail.com>
2022-10-30 19:14:57 +0000

    added css for flipping animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import SingleCard from "./Components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png",matched: false},
+  { src: "/img/potion-1.png",matched: false },
+  { src: "/img/ring-1.png",matched: false },
+  { src: "/img/scroll-1.png",matched: false },
+  { src: "/img/shield-1.png",matched: false },
+  { src: "/img/sword-1.png",matched: false } 
 ];
 
 function App() {
@@ -35,14 +35,23 @@ useEffect(() => {
   if (choiceOne && choiceTwo) {
 
     if(choiceOne.src===choiceTwo.src){
-      console.log(`those cards match`)
+      setCards(prevCards => {
+        return prevCards.map(card => {
+          if (card.src === choiceOne.src){
+            return {...card, matched:true}
+          }else{
+            return card
+          }
+        })
+      })
       resetTurn()
     } else {
-      console.log(`those cards do not match`)
-      resetTurn()
+      setTimeout(() =>  resetTurn(),1000)
     }
   }
 }, [choiceOne, choiceTwo])
+
+console.log(cards)
 
 const resetTurn = () => {
   setChoiceOne(null)
@@ -61,7 +70,9 @@ const resetTurn = () => {
           <SingleCard 
           key={card.id} 
           card={card} 
-          handleChoice={handleChoice} />
+          handleChoice={handleChoice}
+          flipped={card===choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
