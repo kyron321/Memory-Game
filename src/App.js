fa6ff6322ccffecba5525f28a774a68965353e75
kyron321 <kds.060902@gmail.com>
2022-10-30 18:26:17 +0000

    Created visual grid of cards

--- src/App.js
+++ src/App.js
@@ -14,22 +14,32 @@ function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
 
-
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
-    setTurns(0)
+    setTurns(0);
   };
 
-  console.log(cards,turns)
+  console.log(cards, turns);
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
